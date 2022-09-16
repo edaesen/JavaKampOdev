cb5e10f8dbd6175144296d69ae3a261b33566943
Eda <113600786+edaesen@users.noreply.github.com>
2022-09-16 17:16:27 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package ödev;
+
+public class Main {
+	
+//5.video
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Java");
+		
+		System.out.println("Merhaba Java, şu an yolun başındayım hevesliyim");
+		
+		System.out.println("ama ya yapamazsam");
+		
+		System.out.println("şaka şaka yapacağım");
+	}
+
+}
